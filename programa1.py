1943c14aea4d644765535d2b9a473586333d049d
jmmendoza <jmmendoza26@ucatolica.edu.co>
2026-03-02 16:43:02 -0500

    Se añadieron etiquetas

--- programa1.py
+++ programa1.py
@@ -1,9 +1,11 @@
+print('Pares:',end=' ')
 for i in range(10):
     if i % 2 == 0:
         print(i, end=' ')
 
 print()
 
+print('Impares:',end=' ')
 for i in range(10):
     if i % 2 != 0:
         print(i, end=' ')
