49f840c4ce88e882d3c8bfd55e65d75d37e1792f
jmmendoza26 <jmmendoza26@ucatolica.edu.co>
2026-04-16 18:40:47 -0500

    Página personal desplegada con FastAPI

---
+++ mypage/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return FileResponse("templates/index.html")
